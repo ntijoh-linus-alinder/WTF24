11b7350536ddc49b46008deaa9681bd463e50b86
Linus-Alinder-nti-johanneberg <linus.alinder@elev.ga.ntig.se>
2024-02-23 14:02:01 +0100

    fixade mappstruktur, skapade formulär för att lägga till nya filmer och fixade så man kan ladda upp bilder

--- src/app.rb
+++ src/app.rb
@@ -9,17 +9,43 @@ class App < Sinatra::Base
     end
 
     get '/' do
-        erb :index
+        redirect '/movies'
     end
+
     get '/movies' do
+        query = '
+        select * FROM movies
+        JOIN movies_geaneras ON movies.id = movies_geaneras.movie_id
+        JOIN geaneras ON movies_geaneras.id = geaneras.id
+        '
+
         @data = db.execute('SELECT * FROM movies')
-        erb :movies
+        erb :'/movies/index'
     end
     
+    get '/movies/new' do
+        erb :'/movies/new'
+    end
+
+    post '/movies' do 
+        title = params['title']
+        description = params['description'] 
+        year = params['year'] 
+        image = params["image"]
+        File.open('public/img/' + image[:filename], "w") do |f|
+            f.write(image[:tempfile].read)
+        end
+
+        query = 'INSERT INTO movies (title,description,year,image) VALUES (?,?,?,?) RETURNING *'
+        result = db.execute(query,title,description,year,"img/"+image[:filename]).first
+        redirect "/movies/#{result['id']}" 
+    end
+
+
 
 
     get '/movies/:id' do
         @data = db.execute('SELECT * FROM movies WHERE id = ?', params[:id])
-        erb :id
+        erb :'/movies/id'
     end
 end
